91bcefa086061b122ca477680ee19c9fdd41ddaf
mars1385 <faceless1385@gmail.com>
2019-07-10 00:00:24 +0430

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 //
 
 module.exports = {
-	// weatherApiKey: 'YOUR_DARKSKY_API_KEY',
-	// geocodeApiKey: 'YOUR_MAP_BOX_API_KEY'
-	weatherApiKey: '9d95ea647579a271de843222ef453dc3',
-	geocodeApiKey: 'pk.eyJ1IjoibWFyczEzODUiLCJhIjoiY2p4ZGE1eXRiMDZ5eDNubXdxOXl2ZjNvbiJ9.Ehr-vHvTFDbzL6IpIH21Wg'
+	weatherApiKey: 'YOUR_DARKSKY_API_KEY',
+	geocodeApiKey: 'YOUR_MAP_BOX_API_KEY'
 };
